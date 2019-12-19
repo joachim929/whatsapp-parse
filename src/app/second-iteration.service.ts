445a4976161e1d8b0b3f2458c4146bd875fe7e77
unknown <joachim@apptimize.nl>
2019-12-19 22:08:14 +0100

    Fixed last error

--- src/app/second-iteration.service.ts
+++ src/app/second-iteration.service.ts
@@ -9,18 +9,12 @@ export class SecondIterationService {
     '*test* ' + // not bold
     '*t* *e*'; // bold
 
-  /**
-   * todo:
-   *  find fix for `*test**test*`
-   */
-
   constructor() {
   }
 
   spacingMethod() {
     const formattedString = [];
 
-    let holding: number;
     if (this.mess.length < 2) {
       console.log('String length is 1: ', this.mess);
       return;
@@ -32,11 +26,15 @@ export class SecondIterationService {
       tArr[j] = this.mess[j];
       if (this.mess[j] === '*') {
         if (typeof tHold === 'undefined') {
-          if (this.mess[j + 1] !== ' ') {
+          if (j === 0) {
+            if (this.mess[j + 1] !== ' ') {
+              tHold = j;
+            }
+          } else if (this.mess[j + 1] !== ' ' && this.mess[j - 1] !== '*') {
             tHold = j;
           }
         } else {
-          if (this.mess[j - 1] !== ' ' && ((j + 1 === this.mess.length) || this.mess[j + 1] === ' ')) {
+          if (((j + 1) === this.mess.length) || this.mess[j + 1] === ' ' || this.mess[j + 1] === '*') {
             tArr[tHold] = '<strong>';
             tArr[j] = '</strong>';
             tHold = undefined;
@@ -46,51 +44,6 @@ export class SecondIterationService {
     }
     console.log(tArr.join(''));
 
-    // for (let i = 0; i < this.mess.length; i++) {
-    //   formattedString[i] = this.mess[i];
-    //   if (this.mess[i] === '*') {
-    //     if (i === 0) {
-    //       if (!this.isStarOrSpace(this.mess[i + 1])) {
-    //         holding = i;
-    //         console.log('holding at ', i);
-    //       }
-    //     } else if (i === this.mess.length - 1) {
-    //       if ((Number(holding) > -1) && !this.isStarOrSpace(this.mess[i - 1])) {
-    //         formattedString[holding] = '<strong>';
-    //         formattedString[i] = '</strong>';
-    //       }
-    //     } else {
-    //
-    //       if (typeof holding !== 'undefined' && this.mess[i + 1] !== ' ') {
-    //         formattedString[holding] = '<strong>';
-    //         formattedString[i] = '</strong>';
-    //         holding = undefined;
-    //         continue;
-    //       }
-    //       if (this.isStarOrSpace(this.mess[i + 1]) && !this.isStarOrSpace(this.mess[i - 1])) {
-    //         if (typeof holding !== 'undefined') {
-    //           console.log('setting');
-    //           formattedString[holding] = '<strong>';
-    //           formattedString[i] = '</strong>';
-    //           holding = undefined;
-    //         } else {
-    //           holding = i;
-    //         }
-    //       }
-    //       if (typeof holding === 'undefined' && !this.isStarOrSpace(this.mess[i + 1])) {
-    //         holding = i;
-    //       }
-    //     }
-    //   }
-    // }
-
-    // console.log(formattedString.join(''));
-    return formattedString.join('');
-  }
-
-  isStarOrSpace(char) {
-    const equalToSpace = char === ' ';
-    const equalToStar = char === '*';
-    return equalToSpace || equalToStar;
+    return tArr.join('');
   }
 }
